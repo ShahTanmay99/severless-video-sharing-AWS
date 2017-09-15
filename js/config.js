627c387eb2ada1def29007fa61c5fd64417ed17f
Tanmay <shahtany99@gmail.com>
2017-09-15 17:05:50 -0400

    config:Node and Endpoints

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var configConstants = {
+    auth0: {
+        domain: 'aws-serverless.auth0.com',
+        clientId: 'vxyda5MTmPv1BDoSJbmKVEW842OsmlEh'
+    },
+    apiBaseUrl: 'https://o4rbed8qb0.execute-api.us-east-1.amazonaws.com/Dev',
+    firebaseUrl: 'https://aws-serverless.firebaseio.com/'
+};
